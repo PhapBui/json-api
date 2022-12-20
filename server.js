1d62b0b0349ed75de62b95daa7c51310af31f431
Phap Bui <bvphap.tk@gmail.com>
2022-12-20 22:41:06 +0700

    add db.json

--- server.js
+++ server.js
@@ -1,19 +1,66 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+
+// Add custom routes before JSON Server router
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+    req.body.updatedAt = Date.now();
+  } else if (req.method === "PATCH") {
+    req.body.updatedAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+router.render = (req, res) => {
+  //Check GET with pagination
+  // If yes, custom output
+
+  const headers = res.getHeaders();
+  const totalCountHeader = headers["x-total-count"];
+
+  if (req.method === "GET" && totalCountHeader) {
+    const queryParams = queryString.parse(req._parsedUrl.query);
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _totalRows: Number.parseInt(totalCountHeader),
+      },
+    };
+
+    return res.jsonp(result);
+  }
+
+  res.jsonp(res.locals.data);
+};
+
+// Add this before server.use(router)
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
